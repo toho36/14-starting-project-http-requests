0b0260a6e3f1e577768e062b84332b19e8c1a22a
toho36 <toho0036@gmail.com>
2023-05-24 14:11:32 +0200

    editing css

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -38,24 +38,46 @@ function Menu() {
 }
 const Wrapper = styled.div`
   margin: 4rem 0rem;
+  padding: 2rem;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 20px;
+  transition: all 0.3s ease;
+
   @media (max-width: 768px) {
     grid-template-columns: repeat(1, 1fr);
   }
 `;
 const Card = styled.div`
   min-height: 25rem;
-
   border-radius: 2rem;
   overflow: hidden;
 
   img {
     border-radius: 2rem;
-    min-height: 10rem;
-
-    max-height: 15rem;
+    // position: absolute;
+    left: 0;
+    width: 15rem;
+    height: 15rem;
+    justify-content: center;
+    align-items: center;
+    object-fit: cover;
+  }
+  p {
+    // position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(10%, 0%);
+    color: white;
+    width: 100%;
+    // text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    // justify-content: center;
+    align-items: center;
   }
 `;
 export default Menu;
